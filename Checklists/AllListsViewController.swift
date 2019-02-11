73372fda71836d96a7f2b2131519324735913a0c
SerganO <sergeu.ostrovetskiu.2001@gmail.com>
2019-02-11 16:29:09 +0200

    LastChange/not work listDetail

--- Checklists/AllListsViewController.swift
+++ Checklists/AllListsViewController.swift
@@ -8,13 +8,41 @@
 
 import UIKit
 
-class AllListsViewController: UITableViewController {
-
+class AllListsViewController: UITableViewController,
+ListDetailViewControllerDelegate {
+    // MARK:- List Detail View Controller Delegates
+    func listDetailViewControllerDidCancel(
+        _ controller: ListDetailViewController) {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    func listDetailViewController(
+        _ controller: ListDetailViewController,
+        didFinishAdding checklist: Checklist) {
+        let newRowIndex = lists.count
+        lists.append(checklist)
+        
+        let indexPath = IndexPath(row: newRowIndex, section: 0)
+        let indexPaths = [indexPath]
+        tableView.insertRows(at: indexPaths, with: .automatic)
+        
+        navigationController?.popViewController(animated: true)
+    }
+    
+    func listDetailViewController(
+        _ controller: ListDetailViewController,
+        didFinishEditing checklist: Checklist) {
+        if let index = lists.index(of: checklist) {
+            let indexPath = IndexPath(row: index, section: 0)
+            if let cell = tableView.cellForRow(at: indexPath) {
+                cell.textLabel!.text = checklist.name
+            }
+        }
+        navigationController?.popViewController(animated: true)
+    }
+    
     let cellIdentifier = "ChecklistCell"
     var lists = [Checklist]()
-    
-    
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -48,8 +76,8 @@ class AllListsViewController: UITableViewController {
                                cellForRowAt indexPath: IndexPath)
         -> UITableViewCell
     {
-        let cell = tableView.dequeueReusableCell(
-            withIdentifier: cellIdentifier, for: indexPath)
+       let cell = tableView.dequeueReusableCell(
+           withIdentifier: cellIdentifier, for: indexPath)
         
             //let cell = makeCell(for: tableView)
             let checklist = lists[indexPath.row]
@@ -72,7 +100,38 @@ class AllListsViewController: UITableViewController {
             let controller = segue.destination
                 as! ChecklistViewController
             controller.checklist = sender as? Checklist
+        }else if segue.identifier == "AddChecklist" {
+        let controller = segue.destination
+            as! ListDetailViewController
+        controller.delegate = self
         }
+        
     }
     
+    override func tableView(
+        _ tableView: UITableView,
+        commit editingStyle: UITableViewCell.EditingStyle,
+        forRowAt indexPath: IndexPath) {
+        lists.remove(at: indexPath.row)
+        
+        let indexPaths = [indexPath]
+        tableView.deleteRows(at: indexPaths, with: .automatic)
+    }
+    
+    
+    override func tableView(_ tableView: UITableView,
+                               accessoryButtonTappedForRowWith indexPath: IndexPath) {
+        
+        let controller = storyboard!.instantiateViewController(
+            withIdentifier: "ListDetailViewController")
+            as! ListDetailViewController
+        controller.delegate = self
+        
+        let checklist = lists[indexPath.row]
+        controller.checklistToEdit = checklist
+        
+        navigationController?.pushViewController(controller,
+                                                 animated: true)
+    }
+
 }
